24878dca2f09949a8bca822e2b62b5c63abe85b2
HirCoir <97632765+HirCoir@users.noreply.github.com>
2024-06-27 15:58:09 -0600

    Update app-without-tts.js

--- app-without-tts.js
+++ app-without-tts.js
@@ -1,26 +1,22 @@
-require('dotenv').config(); // Para cargar las variables de entorno desde el archivo .env
 const fs = require('fs');
 const venom = require('venom-bot');
 const axios = require('axios');
+require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
+
+const userStates = {}; // Objeto para almacenar el estado del bot para cada usuario
 
 venom
   .create({
-    session: 'session-name' // nombre de la sesión
+    session: 'sesion_venom' // nombre de la sesión
   })
   .then((client) => start(client))
   .catch((error) => {
     console.log(error);
   });
 
-// Función para guardar el chat del usuario en un archivo JSON
-function saveChat(message, conversationMessages) {
-  const filePath = `chats/${message.from}.json`;
-  fs.writeFileSync(filePath, JSON.stringify(conversationMessages, null, 2)); // Pretty print
-}
-
 // Función para cargar el chat del usuario desde un archivo JSON
-function loadChat(message) {
-  const filePath = `chats/${message.from}.json`;
+function loadChat(userId) {
+  const filePath = `chats/${userId}.json`;
   if (fs.existsSync(filePath)) {
     const data = fs.readFileSync(filePath, 'utf8');
     return JSON.parse(data);
@@ -28,67 +24,106 @@ function loadChat(message) {
   return [];
 }
 
-// Función para iniciar el bot y manejar los mensajes
+// Función para eliminar el ID del formato de respuesta
+function cleanAssistantResponse(response) {
+  const regex = /^\$@\$.+?\$@\$/;
+  return response.replace(regex, '').trim();
+}
+
+// Función para guardar el número de un usuario en un archivo JSON
+function saveUserNumber(number) {
+  const filePath = `users/${number}.json`;
+  const userData = { number };
+  fs.writeFileSync(filePath, JSON.stringify(userData, null, 2));
+}
+
 async function start(client) {
   client.onMessage(async (message) => {
-    if (!message.isGroupMsg) {
-      // Comprobamos si el mensaje es para limpiar el historial
-      if (message.body.toLowerCase() === '/clear') {
-        const filePath = `chats/${message.from}.json`;
-        if (fs.existsSync(filePath)) {
-          fs.unlinkSync(filePath); // Eliminamos el archivo del historial
-          await client.sendText(message.from, '¡Historial eliminado con éxito!');
-        } else {
-          await client.sendText(message.from, 'No hay historial que eliminar.');
+    if (message.isGroupMsg) return; // Verificamos si el mensaje es de un grupo
+
+    const userId = message.sender.id; // Usamos el ID del remitente para identificar al usuario
+
+    // Guardamos el número del usuario
+    saveUserNumber(userId);
+
+    // Validamos que el mensaje tenga un cuerpo antes de continuar
+    if (!message.body || typeof message.body !== 'string') {
+      console.warn(`Mensaje sin cuerpo recibido de ${userId}`);
+      return;
+    }
+
+    // Comandos para activar o desactivar el bot
+    const messageBodyLower = message.body.toLowerCase();
+    if (messageBodyLower === '/iniciar') {
+      userStates[userId] = true;
+      await client.sendText(message.from, '¡Bot activado! Escribe un mensaje a VenomBot.\n\nComandos de VenomBot:\n/bye: Terminar conversación.\n/limpiar: Eliminar chats con VenomBot');
+      return;
+    } else if (messageBodyLower === '/bye') {
+      userStates[userId] = false;
+      await client.sendText(message.from, '¡Bot desactivado!');
+      return;
+    } else if (messageBodyLower === '/limpiar') {
+      const filePath = `chats/${userId}.json`;
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath); // Eliminamos el archivo del historial
+        await client.sendText(message.from, '¡Historial eliminado con éxito!');
+      } else {
+        await client.sendText(message.from, 'No hay historial que eliminar.');
+      }
+      return;
+    }
+
+    // Si el bot está desactivado para el usuario, no responde a otros mensajes
+    if (!userStates[userId]) return;
+
+    // Cargamos el chat del usuario desde el archivo JSON
+    let conversationMessages = loadChat(userId);
+
+    conversationMessages.push({ role: 'user', content: message.body });
+
+    // Limitamos el número de mensajes almacenados a 30
+    if (conversationMessages.length > 30) {
+      conversationMessages.shift(); // Eliminamos el mensaje más antiguo
+    }
+
+    // Guardamos el chat del usuario en un archivo JSON
+    const filePath = `chats/${userId}.json`;
+    fs.writeFileSync(filePath, JSON.stringify(conversationMessages));
+
+    try {
+      // Definimos el mensaje de sistema
+      const systemMessage = {
+        role: 'system',
+        content: `Eres un asistente amable y directo en sus respuestas.`
+      };
+
+      // Agregamos el mensaje de sistema antes de hacer la solicitud a la API
+      const apiMessages = [systemMessage, ...conversationMessages];
+
+      // Obtenemos la respuesta del asistente desde la API
+      const response = await axios.post(process.env.OPENAI_SERVER, {
+        model: process.env.MODEL,
+        messages: apiMessages
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.TOKEN}`
         }
-        return;
-      }
+      });
 
-      // Cargamos el chat del usuario desde el archivo JSON
-      let conversationMessages = loadChat(message);
+      // Extraemos el contenido de la respuesta del asistente
+      const assistantResponse = response.data.choices[0].message.content;
+      const cleanedResponse = cleanAssistantResponse(assistantResponse);
 
-      // Agregamos el nuevo mensaje del usuario al arreglo
-      conversationMessages.push({ role: 'user', content: message.body });
+      // Guardamos la respuesta del asistente en el chat del usuario
+      conversationMessages.push({ role: 'assistant', content: cleanedResponse });
+      fs.writeFileSync(filePath, JSON.stringify(conversationMessages));
 
-      // Limitamos el número de mensajes almacenados a 10
-      if (conversationMessages.length > 10) {
-        conversationMessages.shift(); // Eliminamos el mensaje más antiguo
-      }
-
-      // Guardamos el chat del usuario en un archivo JSON
-      saveChat(message, conversationMessages);
-
-      // Ejecutamos el comando curl utilizando axios
-      try {
-        const response = await axios.post(process.env.OPENAI_SERVER, {
-          model: process.env.MODEL,
-          messages: conversationMessages
-        }, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.TOKEN}`
-          }
-        });
-
-        // Extraemos el contenido de la respuesta del asistente
-        const assistantResponse = response.data.choices[0].message.content;
-
-        // Agregamos la respuesta del asistente al arreglo
-        conversationMessages.push({ role: 'assistant', content: assistantResponse });
-
-        // Limitamos el número de mensajes almacenados a 10
-        if (conversationMessages.length > 10) {
-          conversationMessages.shift(); // Eliminamos el mensaje más antiguo
-        }
-
-        // Guardamos el chat del usuario actualizado en un archivo JSON
-        saveChat(message, conversationMessages);
-
-        // Enviamos la respuesta del asistente al usuario
-        await client.sendText(message.from, assistantResponse);
-      } catch (error) {
-        console.error('Error al realizar la solicitud a la API:', error);
-      }
+      // Enviamos la respuesta del asistente al usuario, citando el mensaje del usuario
+      await client.reply(message.from, cleanedResponse, message.id);
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+      await client.sendText(message.from, '¡Lo siento! Hubo un error al procesar su solicitud.');
     }
   });
 }
